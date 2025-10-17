3ac39251a68e3f593e043e1415ef8dc5e05a1666
Kate Korobova <korkat1611@gmail.com>
2025-10-17 15:13:35 +0300

    add Math.round

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        double index = weight / (height * height);
+        double index = Math.round((weight / (height * height)) * 100.0) / 100.0;
         return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double index = service.calculate(55.1, 1.85);
+        double index = service.calculate(55.133333, 1.85);
         System.out.println(index);
+
     }
 }
